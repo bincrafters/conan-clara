62ff966651011bad4adc403ae1757db2cde1edac
Jerry W <jerrywiltse@gmail.com>
2018-01-18 14:00:53 -0500

    set version back to 1.0.0 per phil

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class ClaraConan(ConanFile):
     name = "clara"
-    version = "1.0"
+    version = "1.0.0"
     description = "A simple to use, composable, command line parser for C++ 11 and beyond"
     url = "https://github.com/bincrafters/conan-libname"
     homepage = "https://github.com/catchorg/Clara"
@@ -23,7 +23,6 @@ class ClaraConan(ConanFile):
 
     def package(self):
         include_folder_src = os.path.join(self.source_subfolder, "single_include")
-        
         self.copy(pattern="LICENSE.txt", dst="license", src=self.source_subfolder)
         self.copy(pattern="*.hpp", dst="include", src=include_folder_src)
 
